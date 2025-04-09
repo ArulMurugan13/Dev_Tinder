9c04900db677688d40cf306c5a421d983da4c8cc
ArulMurugan13 <arulking2785@gmail.com>
2025-04-09 18:04:30 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -4,100 +4,112 @@ const express = require("express");
 const app = express();
 
 //importing db connction
-const connectDb = require("./config/database.js")
+const connectDb = require("./config/database.js");
 const User = require("./models/users.js");
 
-//middleware - no path - runs for all - 
+//middleware - no path - runs for all -
 //helps to convert json to js obj
 app.use(express.json());
 
-
 //post api
-app.post("/signup",async (req,res)=>{
+app.post("/signup", async (req, res) => {
   // {fname:"Arivu",lname:"Selvan",age:23,city:"Ariyalur"}
-  const newUserObj = new User(req.body);
-  await newUserObj.save();
 
-  res.status(200).send("User Added Successfully");
+  try {
+    const newUserObj = new User(req.body);
+    await newUserObj.validate();
+    await newUserObj.save();
+    res.status(200).send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 });
 
-//get all user 
-app.get("/getUser",async (req,res)=>{
-    
-    const data = await  User.find();
-    res.status(200).send(data);
-
+//get all user
+app.get("/getUser", async (req, res) => {
+  const data = await User.find();
+  res.status(200).send(data);
 });
-//get  user by fname 
-app.get("/getUserByName",async (req,res)=>{
-    
-    const  name = req.body.fname;
+//get  user by fname
+app.get("/getUserByName", async (req, res) => {
+  const name = req.body.fname;
 
-    try{
-       const data = await User.findOne({ fname : name });
+  try {
+    const data = await User.findOne({ fname: name });
 
-       if(data)
-       {
-         res.status(200).send(data);
-       }
-       else
-       {
-        res.status(401).send("Unable to fetch by ", name);
-       }
+    if (data) {
+      res.status(200).send(data);
+    } else {
+      res.status(401).send("Unable to fetch by ", name);
     }
-    catch(err)
-    {
-      console.log("Something went wrong")
-      res.status(400).send( err.message)
+  } catch (err) {
+    console.log("Something went wrong");
+    res.status(400).send(err.message);
+  }
+});
+
+//get user by id from url
+app.get("/getUser/:userId", async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const data = await User.findOne({_id:userId});
+
+    if (data) {
+      res.status(200).send(data);
+    } else {
+      res.status(401).send("Unable to fetch by ", name);
     }
+  } catch (err) {
+    console.log("Something went wrong");
+    res.status(400).send(err.message);
+  }
 });
 
 //Delete api
-app.delete("/user" ,async (req,res)=>{
+app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
   const deleteUser = await User.findByIdAndDelete(userId);
   res.send("User Deleted Successfully");
 });
 //delete all document
-app.delete("/deleteAllUser" , async (req,res)=>{
-      const deleteAllData = await User.deleteMany({});
-      res.send("All Data are deleted...")
+app.delete("/deleteAllUser", async (req, res) => {
+  const deleteAllData = await User.deleteMany({});
+  res.send("All Data are deleted...");
 });
 
 //adding a field
-app.patch("/addEmailField" , async (req,res)=>{
-
-  const addingField = await User.updateMany({},{$set : {email : "abc@gmail.com"}});
-  res.send("Email field added successfully")   
+app.patch("/addEmailField", async (req, res) => {
+  const addingField = await User.updateMany(
+    {},
+    { $set: { email: "abc@gmail.com" } }
+  );
+  res.send("Email field added successfully");
 });
 
 //updating email by id
 
-app.patch("/updateEmail", async (req,res)=>{
+app.patch("/updateEmail", async (req, res) => {
   const userId = req.body.userId;
-  const user = await User.findOne({_id:userId});
+  const user = await User.findOne({ _id: userId });
   const username = user?.fname;
   // updateEmail
-   await User.findByIdAndUpdate(userId , {$set : {email : username+"@gmail.com"}});
+  await User.findByIdAndUpdate(userId, {
+    $set: { email: username + "@gmail.com" },
+  });
 
   res.send("Email id updated successfully");
 });
 
+connectDb()
+  .then(() => {
+    console.log("DB is connected successfully");
+    app.listen(3000, () => {
+      console.log("Server Starts Running... listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Something went wrong db is not cponnected");
+  });
 
-
-connectDb().then( ()=>{
-        console.log("DB is connected successfully");
-        app.listen(3000, () => {
-          console.log("Server Starts Running... listening on port 3000");
-        });
-        
-        }).catch( (err)=>{
-             console.log("Something went wrong db is not cponnected");
-});
-
-app.use("/user",(req,res)=>{
-
-});
-
-
-
+app.use("/user", (req, res) => {});
--- src/models/users.js
+++ src/models/users.js
@@ -1,13 +1,38 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  fname: { type: String },
-  lname: { type: String },
-  age: { type: Number },
-  city: { type: String },
-  email: { type: String },
-});
+//schema validation
+const userSchema = new mongoose.Schema(
+  {
+    fname: {
+      type: String,
+      required: true,
+    },
+    lname: { type: String },
+    age: { type: Number },
+    city: { type: String },
+    email: { type: String, required: true, lowercase: true, unique: true,
+    validate(value){
+      if( !validator.isEmail(value)){
+        throw new Error("Invalid Email")
+      };
+    }
+   },
+   password:{type:String,required:true,
+    validate(value){
+       if(!validator.isStrongPassword(value))
+       {
+        throw new Error("Password Must be strong")
+       }
+    }
+   }
+  },
+  { timestamps: true,
+   }
+);
 
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
+
+//timeStamps - provides -> crateat ,updateAt
